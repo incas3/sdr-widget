8384f91c745f2e38b5cf2d00aef059ec62ebb737
Erik Kallen <info@erikkallen.nl>
2012-07-09 15:39:16 +0200

    single stereo input slave i2s channel

--- src/uac1_image.c
+++ src/uac1_image.c
@@ -78,7 +78,7 @@ static void x_image_task_init(void) {
   vStartTaskPowerDisplay();
   vStartTaskPushButtonMenu();
 #endif
-  vStartTaskMoboCtrl();
+  //vStartTaskMoboCtrl();
   // vStartTaskEXERCISE( tskIDLE_PRIORITY );
   uac1_AK5394A_task_init();
   device_mouse_hid_task_init(UAC1_EP_HID_RX, UAC1_EP_HID_TX);
